ad2e98ece2d16d4696ea393329e784f878db3fb8
Masoud Karimi <masoud.karimi7171@outlook.com>
2023-09-17 17:05:34 +0330

    feat: adds loadtest for kafka

--- KafkaTest/MessageConsumer.cs
+++ KafkaTest/MessageConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using Confluent.Kafka;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace KafkaTest;
 public class MessageConsumer
@@ -11,18 +12,20 @@ public class MessageConsumer
         _bootstrapServers = bootstrapServers;
     }
 
-    public async Task load(string topicName, int number, int workers)
+    public async Task load(string topicName, int number, int workers, string[] partitions)
     {
         List<int> keys = Enumerable.Range(1, number).ToList();
         await Parallel.ForEachAsync(keys, new ParallelOptions() { MaxDegreeOfParallelism = workers },
         (item, cancelToken) =>
         {
-            return Consume(topicName + "_" + item.ToString());
+            int numberOfPartitions = 1;
+            int.TryParse(partitions[item - 1], out numberOfPartitions);
+            return Consume(topicName + "_" + item.ToString(), numberOfPartitions);
         });
     }
 
 
-    private async ValueTask Consume(string topic)
+    private async ValueTask Consume(string topic, int partitions)
     {
         var config = new ConsumerConfig
         {
@@ -38,36 +41,41 @@ public class MessageConsumer
             cts.Cancel();
         };
 
-        using (var consumer = new ConsumerBuilder<string, string>(
-            config).SetLogHandler((_, message) => LogCallBack(message, config)).Build())
+        List<int> keys = Enumerable.Range(1, partitions).ToList();
+        Parallel.ForEach(keys, key =>
         {
-            consumer.Subscribe(topic);
-            try
+            using (var consumer = new ConsumerBuilder<string, string>(
+                        config).SetLogHandler((_, message) => LogCallBack(message, config)).Build())
             {
-                int counter = 0;
-                while (true)
+                consumer.Subscribe(topic);
+                try
                 {
-                    Interlocked.Increment(ref counter);
-                    var cr = consumer.Consume(cts.Token);
-                    if (counter % 1000 == 0)
-                        Console.WriteLine($"Consumed event from topic {topic} value {cr.Message.Value}");
+                    int counter = 0;
+                    while (!Console.KeyAvailable)
+                    {
+                        Interlocked.Increment(ref counter);
+                        var cr = consumer.Consume(cts.Token);
+                        if (counter % 1000 == 0)
+                            Console.WriteLine($"Consumed event from topic {topic} and partiotion {cr.Partition.Value} value {cr.Message.Value}");
 
+                    }
+                }
+
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    consumer.Close();
                 }
             }
 
-            catch (OperationCanceledException)
-            {
-                // Ctrl-C was pressed.
-            }
-            catch (Exception)
-            {
-
-            }
-            finally
-            {
-                consumer.Close();
-            }
-        }
+        });
     }
 
     private static void LogCallBack(LogMessage message, ConsumerConfig config)
--- KafkaTest/Program.cs
+++ KafkaTest/Program.cs
@@ -2,6 +2,7 @@
 using Confluent.Kafka;
 using Confluent.Kafka.Admin;
 using KafkaTest;
+using System.Collections.Concurrent;
 
 public class Program
 {
@@ -26,32 +27,42 @@ public class Program
         int worker = string.IsNullOrWhiteSpace(Worker) ? 8 : Convert.ToInt32(Worker);
 
         int number = string.IsNullOrWhiteSpace(numberOfTopics) ? 1 : Convert.ToInt32(numberOfTopics);
+
         string[] partitions = numberOfPartitions.Split(',');
+        if (partitions.Length < number)
+        {
+            GenerateDefaultPartitions(ref numberOfPartitions, number, ref partitions);
+        }
+
         string[] replicas = replicationFactors.Split(',');
+        if (replicas.Length < number)
+        {
+            GenerateDefaultReplicas(ref replicationFactors, number, ref replicas);
+
+        }
 
         for (int i = 1; i <= number; i++)
         {
             try
             {
-                await CreateTopicIfNotExists(BootstrapServers, topicPrefixName + "_" + i.ToString(), Convert.ToInt32(partitions[i - 1]) == 0 ? 1 : Convert.ToInt32(partitions[i - 1]),
-                Convert.ToInt32(replicas[i - 1]) == 0 ? 1 : Convert.ToInt32(replicas[i - 1]));
+                await CreateTopicIfNotExists(BootstrapServers, topicPrefixName + "_" + i.ToString(), Convert.ToInt32(partitions[i - 1]) < 1 ? 1 : Convert.ToInt32(partitions[i - 1]),
+                Convert.ToInt32(replicas[i - 1]) < 1 ? 1 : Convert.ToInt32(replicas[i - 1]));
             }
             catch (IndexOutOfRangeException)
             {
                 await CreateTopicIfNotExists(BootstrapServers, topicPrefixName + "_" + i.ToString(), 1, 1);
             }
         }
-            
+
 
         var messageProducer = new MessageProducer(BootstrapServers);
         var messageConsumer = new MessageConsumer(BootstrapServers);
 
-        if (Mode == "1")
-            await messageProducer.load(topicPrefixName, number, worker);
-        else if (Mode == "2") await messageConsumer.load(topicPrefixName, number, worker);
+        if (Mode == "1") await messageProducer.load(topicPrefixName, number, worker);
+        else if (Mode == "2") await messageConsumer.load(topicPrefixName, number, worker, partitions);
         else if (Mode == "3")
         {
-            Task[] tasks = { messageProducer.load(topicPrefixName, number, worker), messageConsumer.load(topicPrefixName, number, worker) };
+            Task[] tasks = { messageProducer.load(topicPrefixName, number, worker), messageConsumer.load(topicPrefixName, number, worker, partitions) };
             Task.WaitAll(tasks);
         }
 
@@ -59,6 +70,28 @@ public class Program
 
         Console.WriteLine("Press any key to exit...");
         Console.ReadLine();
+    }
+
+    private static void GenerateDefaultReplicas(ref string? replicationFactors, int number, ref string[] replicas)
+    {
+        if (string.IsNullOrEmpty(replicationFactors))
+            replicationFactors = string.Join(",", Enumerable.Repeat("1", number - (replicas.Length - 1)));
+        else if (replicationFactors.EndsWith(','))
+            replicationFactors += string.Join(",", Enumerable.Repeat("1", number - (replicas.Length-1)));
+        else
+            replicationFactors = string.Concat(replicationFactors, ",", string.Join(",", Enumerable.Repeat("1", number - replicas.Length)));
+        replicas = replicationFactors.Split(',');
+    }
+
+    private static void GenerateDefaultPartitions(ref string? numberOfPartitions, int number, ref string[] partitions)
+    {
+        if (string.IsNullOrEmpty(numberOfPartitions))
+            numberOfPartitions = string.Join(",", Enumerable.Repeat("1", number - (partitions.Length - 1)));
+        else if (numberOfPartitions.EndsWith(','))
+            numberOfPartitions += string.Join(",", Enumerable.Repeat("1", number - (partitions.Length -1)));
+        else
+            numberOfPartitions = string.Concat(numberOfPartitions, ",", string.Join(",", Enumerable.Repeat("1", number - partitions.Length)));
+        partitions = numberOfPartitions.Split(',');
     }
 
     private static async Task CreateTopicIfNotExists(string bootstrapServers, string topicName, int partitions, int replicationFactor)
